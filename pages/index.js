105e2e09aeb2ef7112a13c42e335063680844969
fpaco974 <136788267+fpaco974@users.noreply.github.com>
2024-04-06 14:14:28 +0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,23 @@
 import React from 'react';
-import Head from 'next/head';
+import Head from 'next/head'
 
 const HomePage = () => {
   return (
-    <div>
+    <div> 
       <Head>
-        <title>le comparateur mariage</title>
+        <title>Le Comparateur Mariage</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <iframe
-          src="https://gamma.app/embed/57zaybrpgn837x1" 
+          src="https://gamma.app/embed/dx6ffp881ka6p3t"
           style={{ width: '100%', height: '100vh', border: 'none' }}
-          allowFullScreen
+          allow="FullScreen"
         ></iframe>
       </main>
     </div>
-  );
+);
 };
 
 export default HomePage;
