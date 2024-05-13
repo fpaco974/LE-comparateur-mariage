09e5ae3b3951959113e8eae2d43688fdec4070da
fpaco974 <136788267+fpaco974@users.noreply.github.com>
2024-05-13 19:04:40 +0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,12 +6,14 @@ const HomePage = () => {
     <div> 
       <Head>
         <title>Le Comparateur Mariage</title>
+        <meta name="description" content="Mariage comparateur des prestataires de mariage prix des services de mariage et selection des meilleurs prestataires mariage et entreprise de mariage." />
         <link rel="icon" href="/favicon.ico" />
+        <link rel="prefetch" href="https://www.le-comparateur-mariage.net" />
       </Head>
 
       <main>
         <iframe
-          src="https://le-comparateur-mariage.net/" 
+          src="https://www.le-comparateur-mariage.net" 
           style={{ width: '100%', height: '100vh', border: 'none' }}
           allow="FullScreen"
         ></iframe>
