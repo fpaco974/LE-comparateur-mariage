8fe05aff99f738f675fcebc908c968c41df0fcf0
fpaco974 <136788267+fpaco974@users.noreply.github.com>
2024-05-28 15:25:02 +0400

    Update basic.cy.js

--- cypress/e2e/basic.cy.js
+++ cypress/e2e/basic.cy.js
@@ -2,16 +2,3 @@ describe('sample test', () => {
   beforeEach(() => {
     cy.visit('/')
   })
-
-  it('displays the resources text', () => {
-    cy.get('h1', {timeout: 6000})
-    .contains('Welcome to my app!');
-  })
-  it('renders the Netlify logo image', () => {
-    cy.get('img', {timeout: 6000})
-    .should('be.visible')
-    .and(($img) => {
-      expect($img[0].naturalWidth).to.be.greaterThan(0);
-    })
-  })
-})
